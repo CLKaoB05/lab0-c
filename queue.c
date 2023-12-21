73e19b246b1464b0e2894bca0df1f6d387af67f5
CLKaoB05 <g935218g@gmail.com>
2023-12-21 14:10:43 +0000

    Implement until q_delete_mid

--- queue.c
+++ queue.c
@@ -77,25 +77,47 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *element = list_first_entry(head, element_t, list);
+    list_del(&element->list);
+    strncpy(sp, element->value, bufsize);
+    return element;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    return q_remove_head(head->prev->prev, sp, bufsize);
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return -1;
+    int size = 0;
+    element_t *entry;
+    list_for_each_entry (entry, head, list)
+        size++;
+    return size;
 }
 
 /* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
-    // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+    struct list_head **indirect = &(head->next);
+    for (struct list_head *fast = head->next;
+         fast != head && fast->next != head; fast = fast->next->next) {
+        indirect = &(*indirect)->next;
+    }
+    element_t *element = list_entry(*indirect, element_t, list);
+    list_del_init(&(element->list));
+    q_release_element(element);
     return true;
 }
 
